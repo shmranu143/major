8d94474883a85ac47ef2ede27e0cadf1238b1d8b
shmranu143 <recs.cse1641@gmail>
2021-02-18 19:45:40 +0530

    initial commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h3>here I will make my major project</h3>
+    </div>
+  );
+}
+
+export default App;
